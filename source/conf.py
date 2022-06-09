469471bc499478db59ba1c0e01b63d02f34e6b8d
Richard Rigby <r.rigby@leeds.ac.uk>
2022-06-09 08:45:03 +0100

    testing autodoc

--- source/conf.py
+++ source/conf.py
@@ -10,9 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../licsar-proc-test/python/LiCSAR_lib'))
+
 
 # Import datetime for date information:
 import datetime
@@ -32,6 +33,7 @@ email = 'email'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.githubpages'
 ]
 
